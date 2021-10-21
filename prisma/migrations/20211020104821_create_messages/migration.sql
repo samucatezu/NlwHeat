544587b351b57793ee69031909371c325b7a2613
Samuel Almeida <dev3.gbmlogistica@outlook.com>
2021-10-21 10:19:43 -0300

    node.js codes

---
+++ prisma/migrations/20211020104821_create_messages/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the `messages` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "messages";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "message" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "text" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_id" TEXT NOT NULL,
+    CONSTRAINT "message_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
